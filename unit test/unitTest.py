02ef37dc1a18daefb403e79ad4a2b98f7518341e
mahdi-ebrahimi-per <76206312+mahdi-ebrahimi-per@users.noreply.github.com>
2021-11-02 18:50:19 +0330

    Update unitTest.py

--- unit test/unitTest.py
+++ unit test/unitTest.py
@@ -1,4 +1,5 @@
 import unittest
+from colorama import Fore, Back, Style
 
 
 class UnitTestTools(unittest.TestCase):
@@ -6,7 +7,15 @@ class UnitTestTools(unittest.TestCase):
     def __init__(self):
         pass
 
-    def equal(self, function, equalTo):
+    def equal(self, function, equalTo, PASS_FALED=True):
+        
+        if PASS_FALED:
+            if function == equalTo:
+                return (Fore.GREEN + "PASS" + Fore.RESET) 
+
+            else:
+                return (Fore.RED + "FALED" + Fore.RESET)
+
         return function == equalTo
 
 
@@ -30,12 +39,17 @@ def same(x):
 
 
 
-
 test = UnitTestTools()
 
-# print(test.equal(sumN(12, 3), 15))
+print(test.equal( sumN(12, 3), 15))
+print(test.equal(sumN(12, 3), 12))
+print(test.equal(sumN(12, 3), 45))
+print(test.equal(sumN(12, 3), 0))
 
-test.funcReaction(sum, [2, 5], [1 , 5])
+
+
+
+# test.funcReaction(sum, [2, 5], [1 , 5])
     # 2 + 1 = 3
     # 2 + 5 = 7
     # 5 + 1 = 6
