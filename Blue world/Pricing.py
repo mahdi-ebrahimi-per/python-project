ab60308fdfd831bbcbb2a1247cd971dbfc3ce65e
mahdi-ebrahimi-per <76206312+mahdi-ebrahimi-per@users.noreply.github.com>
2021-12-19 21:24:33 +0330

    Create Pricing.py

---
+++ Blue world/Pricing.py
@@ -0,0 +1,18 @@
+
+maxSupply = 3000
+allDemand = 60
+
+numOfConsumer = 4
+eachConsumerMaxUsage = allDemand / numOfConsumer
+
+
+print( 1 / (maxSupply/allDemand) )
+
+# for A in list(range(1000+1))[::100]:
+#     try:
+#         print(f"Eeach litr of water price of {A}L usage : {1 / (maxSupply/A) *10}" )
+
+#     except:
+#         pass
+
+
